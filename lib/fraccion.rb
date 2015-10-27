2147773ec8f550da05ed71638addf5e20a65a0a6
Eleazar Díaz Delgado <eleazardzdo@gmail.com>
2015-10-27 19:20:54 +0000

    implementado el gcd

--- lib/fraccion.rb
+++ lib/fraccion.rb
@@ -1,9 +1,17 @@
 class Fraccion
-    attr_accessor :numer, :denom
+    attr_reader :numer, :denom
+
+    def gcd(x,y)
+        if y==0 then x
+        else gcd(y,x%y)
+        end
+    end
 
     def initialize(numer, denom)
-        @numer = numer
-        @denom = denom
+        raise ZeroDivisionError, " denominator is cero " if denom.eql? 0
+        d = gcd(numer, denom)
+        @numer = numer / d
+        @denom = denom / d
     end
 
     def to_s
--- test/tc_fraccion.rb
+++ test/tc_fraccion.rb
@@ -9,5 +9,7 @@ class TestFraccion < Test::Unit::TestCase
 
     def test_simple
         assert_equal("1/2", @frac1.to_s)
+        assert_equal("1/1", Fraccion.new(5,5).to_s)
+        # assert_raise_with_name(ZeroDivisionError,"",Fraccion.new(4,0))
     end
 end
